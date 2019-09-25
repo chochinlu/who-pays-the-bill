5c04202e0c1fe3e7b91b0873f15c4b6e31e9aaa9
Park.Lu <park.lu@nogle.com>
2019-09-25 17:49:51 +0800

    Add more functions

--- js/main.js
+++ js/main.js
@@ -3,20 +3,29 @@ new Vue({
   data: {
     inputName: '',
     selected: '',
-    names: []
+    names: [],
+    hasError: false
   },
   methods: {
     addName() {
-      this.names.push(this.inputName)
-      this.inputName = ''
-      this.$refs.content.focus()
+      if (this.inputName === '') {
+        this.hasError = true
+      } else {
+        this.hasError = false
+        this.names.push(this.inputName)
+        this.inputName = ''
+        this.$refs.content.focus()
+      }
+    },
+    removeName(index) {
+      this.names.splice(index, 1)
     },
     choose() {
       this.selected = this.names[Math.floor(Math.random() * this.names.length)]
       this.names = []
     },
-    playAgain(){
-        this.selected = ''
+    playAgain() {
+      this.selected = ''
     }
   }
 })
